4e3ca67a882ad2c5257383ab388307740f48718c
Kamil Aydin <kaydin.zk@gmail.com>
2025-01-12 04:12:07 +0100

    init

---
+++ src/main/java/com/reson8/app/Reson8Application.java
@@ -0,0 +1,13 @@
+package com.reson8.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reson8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reson8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/reson8/app/Reson8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reson8.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reson8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
