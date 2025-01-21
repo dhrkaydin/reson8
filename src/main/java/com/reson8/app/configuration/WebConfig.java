70907140b12faa82d24dbf42c4dc8f3262730351
Kamil Aydin <kaydin.zk@gmail.com>
2025-01-21 13:58:50 +0100

    Updated Cors mappings

--- src/main/java/com/reson8/app/configuration/WebConfig.java
+++ src/main/java/com/reson8/app/configuration/WebConfig.java
@@ -1,18 +1,24 @@
 package com.reson8.app.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/api/**")
-        .allowedOrigins("http://localhost:5713", "http://localhost:5714", "http://localhost:8080")  // React app URL
-        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-        .allowedHeaders("*")
-        .allowCredentials(true);
+public class WebConfig {
 
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+            .allowedOrigins("http://localhost:5173")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+      }
+    };
   }
 }
